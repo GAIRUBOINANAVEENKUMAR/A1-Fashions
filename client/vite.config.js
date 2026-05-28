78d0d73c604e96d389bf3c5cfbd3c95ec3654d50
GAIRUBOINANAVEENKUMAR <gairuboina.naveenkumar45@gmail.com>
2026-05-28 14:15:26 +0530

    back

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'https://a1-fashions.onrender.com',
+                target: 'http://localhost:5000',
                 changeOrigin: true,
             },
         },
